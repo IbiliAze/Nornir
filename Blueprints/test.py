abb405f8a6449d6c1af5a7b00884899347cc8626
Ibi Hasanli <ibili73@gmail.com>
2021-01-02 11:20:00 +0000

    initial commit

---
+++ Blueprints/test.py
@@ -0,0 +1,12 @@
+from nornir import InitNornir
+# from nornir.plugins.functions.text import print_result
+# from nornir.plugins.tasks.networking import netmiko_send_commands
+
+nr = InitNornir(config_file='config.yaml')
+
+# def show_command(task):
+#     showVer = task.run(netmiko_send_commands, command_string="show ver")
+#     result = showVer.result
+#     print(result)
+
+# nr.run(task=show_command)
