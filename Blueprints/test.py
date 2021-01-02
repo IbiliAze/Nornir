76a0da69edc34c296877e27c9626fe4a107a4859
Ibi Hasanli <ibili73@gmail.com>
2021-01-02 03:31:12 -0800

    aliased commit on ubuntu

--- Blueprints/test.py
+++ Blueprints/test.py
@@ -2,7 +2,7 @@ from nornir import InitNornir
 # from nornir.plugins.functions.text import print_result
 # from nornir.plugins.tasks.networking import netmiko_send_commands
 
-nr = InitNornir(config_file='config.yaml')
+nr = InitNornir(config_file='/home/ibi/Documents/Git/Nornir/Blueprints/config.yaml')
 
 # def show_command(task):
 #     showVer = task.run(netmiko_send_commands, command_string="show ver")
