f66e44e989ee4e588aa81cfdcb2ddae3eb3c0f08
Ibi Hasanli <ibili73@gmail.com>
2021-01-02 04:06:29 -0800

    aliased commit on ubuntu

--- Blueprints/test.py
+++ Blueprints/test.py
@@ -1,12 +1,21 @@
+###########################################################################MODULES
 from nornir import InitNornir
-# from nornir.plugins.functions.text import print_result
-# from nornir.plugins.tasks.networking import netmiko_send_commands
+###########################################################################PLUGINS
+from nornir_utils.plugins.tasks.data import load_yaml
+from nornir_utils.plugins.tasks.files import write_file
+from nornir_utils.plugins.functions import print_result
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_jinja2.plugins.tasks import template_file
+from nornir_netmiko.tasks import netmiko_send_config, netmiko_send_command
+##################################################################################
+
+
 
 nr = InitNornir(config_file='/home/ibi/Documents/Git/Nornir/Blueprints/config.yaml')
 
-# def show_command(task):
-#     showVer = task.run(netmiko_send_commands, command_string="show ver")
-#     result = showVer.result
-#     print(result)
+def show_command(task):
+    showVer = task.run(netmiko_send_command, command_string="show ver")
+    result = showVer.result
+    print(result)
 
-# nr.run(task=show_command)
+nr.run(task=show_command)
